81521a99ffcebd4852f20ae2fda6e647a7869b00
majahar <majhar777@gmail.com>
2019-04-11 15:38:08 +0530

    adding some file

---
+++ server.js
@@ -0,0 +1,29 @@
+/*var http = require('http');
+
+http.createServer( function(req,res){
+
+	res.writeHead(200,{"Content-type":"text/plain"});
+	res.end("hello");
+
+}).listen(1337,'127.0.0.1');
+
+console.log("Server has started");*/
+
+
+var http = require('http');
+var fs= require('fs');
+
+http.createServer( function(req,res){
+     fs.readFile('index.html',function(error,data){
+
+      res.writeHead(200,{"Content-type":"text/html"});
+
+	res.end(data);
+
+     });
+
+	
+
+}).listen(1337,'127.0.0.1');
+
+console.log("Server has started");
